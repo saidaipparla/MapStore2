41dfd711369c237a3fabf1d46f3a7f94fff755cb
saidaipparla <saidarao1214@gmail.com>
2016-12-19 11:47:30 +0100

    fix

--- web/client/plugins/MousePosition.jsx
+++ web/client/plugins/MousePosition.jsx
@@ -9,7 +9,7 @@
    * Plugins
    * @overview For detecting MousePosition On Map and display its x and y coordinates.
    * @name MousePositionPlugin module
-   * @module Plugins/MousePositionPlugin
+   * @module  MousePositionPlugin
    * @requires react
    * @requires react-redux
    * @requires selectors/map
