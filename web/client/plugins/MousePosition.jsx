b5e4a50df7b833ec247591ac0d871d0514b99cb1
saidaipparla <saidarao1214@gmail.com>
2016-12-20 15:30:18 +0100

    fix24

--- web/client/plugins/MousePosition.jsx
+++ web/client/plugins/MousePosition.jsx
@@ -20,7 +20,7 @@ const {createSelector} = require('reselect');
 const assign = require('object-assign');
 
 const {changeMousePositionCrs, changeMousePositionState} = require('../actions/mousePosition');
-/** @lends module:plugins.MousePositionPluginw# */
+
 const getDesiredPosition = (map, mousePosition, mapInfo) => {
     if (mousePosition.showCenter && map) {
         return map.center;
@@ -39,12 +39,7 @@ const getDesiredPosition = (map, mousePosition, mapInfo) => {
     }
     return mousePosition.position;
 };
-/**
-  * Override to transform the property value before it is stored.
-  *
-  * @return Promise | String
-  * @memberof module:plugins:MousePosition:selector
-  */
+
 const selector = createSelector([
     mapSelector,
     (state) => state.mousePosition || {},
